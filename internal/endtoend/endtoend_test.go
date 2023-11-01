cd83d45feeb45b6e6ccf25e3db87e5a54dadf24b
Kyle Conroy <kyle@conroy.org>
2023-11-01 15:51:05 -0700

    Gross, but necessary

--- internal/endtoend/endtoend_test.go
+++ internal/endtoend/endtoend_test.go
@@ -1,9 +1,13 @@
 package endtoend
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -57,12 +61,41 @@ func ExpectedOutput(t *testing.T, dir string) []byte {
 	return output
 }
 
+var pattern = regexp.MustCompile(`sha256: ".*"`)
+
 func TestGenerate(t *testing.T) {
+	// The SHA256 is required, so we calculate it and then update all of the
+	// sqlc.yaml files.
+	// TODO: Remove this once sqlc v1.24.0 has been released
+	wasmpath := filepath.Join("..", "..", "bin", "sqlc-gen-python.wasm")
+	if _, err := os.Stat(wasmpath); err != nil {
+		t.Fatalf("sqlc-gen-python.wasm not found: %s", err)
+	}
+	wmod, err := os.ReadFile(wasmpath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sum := sha256.Sum256(wmod)
+	sha256 := fmt.Sprintf("%x", sum)
+
 	sqlc := LookPath(t, "sqlc-dev", "sqlc")
 
 	for _, dir := range FindTests(t, "testdata") {
 		dir := dir
 		t.Run(dir, func(t *testing.T) {
+			// Check if sqlc.yaml has the correct SHA256 for the plugin. If not, update the file
+			// TODO: Remove this once sqlc v1.24.0 has been released
+			yaml, err := os.ReadFile(filepath.Join(dir, "sqlc.yaml"))
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !bytes.Contains(yaml, []byte(sha256)) {
+				yaml = pattern.ReplaceAllLiteral(yaml, []byte(`sha256: "`+sha256+`"`))
+				if err := os.WriteFile(filepath.Join(dir, "sqlc.yaml"), yaml, 0644); err != nil {
+					t.Fatal(err)
+				}
+			}
+
 			want := ExpectedOutput(t, dir)
 			cmd := exec.Command(sqlc, "diff")
 			cmd.Dir = dir
